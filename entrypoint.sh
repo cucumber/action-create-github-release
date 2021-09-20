635818717c611d18fb58e6763e164f3b34f52ee6
Matt Wynne <matt@cucumber.io>
2021-09-20 13:04:37 -0700

    args

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ next_version=$([[ "$next_version" == There* ]] && (echo $next_version && exit 1)
 echo "Next version: '$next_version'"
 
 # Get release notes from CHANGELOG
-body=$(changelog show "$next_version_heading" --filename CHANGELOG.md --output)
+body=$(changelog show "$next_version_heading")
 
 # Create GitHub Release
 sha=$GITHUB_SHA
