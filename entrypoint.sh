aff0a6050805c3dd1a0b8d7862f666549fced4a3
Matt Wynne <matt@cucumber.io>
2021-09-20 22:46:03 -0700

    Work out the body

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ next_version=$([[ "$next_version" == There* ]] && (echo $next_version && exit 1)
 echo "Next version: '$next_version'"
 
 # Get release notes from CHANGELOG
-body='$(changelog show "$next_version_heading")'
+body=$(changelog show "$next_version_heading")
 body=${body//$'\n'/\\n}
 body=${body//$'`'/\\\`}
 echo "body:"
